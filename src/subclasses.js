2613b02b1ec2d265e16f2ca32e1ba97ccf0a2c07
NicKAnderson038 <nickanderson@gmail.com>
2018-10-09 21:25:37 -0700

    .

--- src/subclasses.js
+++ src/subclasses.js
@@ -15,3 +15,21 @@ const book1 = new Book(
   "2017"
 );
 console.log(book1.combine());
+
+// Subclasses - react style
+class TVshow extends Book {
+  constructor(title, author, year, season) {
+    super(title, author, year);
+    this.season = season;
+  }
+  combine() {
+    return console.log(
+      `${this.title} by ${this.author}, season ${this.season} in ${this.year}`
+    );
+  }
+}
+
+// Instantiate TVshow
+const tv1 = new TVshow("The Expance", "James S. A. Corey", "2017", 1);
+
+console.log(tv1.combine());
